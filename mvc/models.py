bfc4a4eb2476609d16dbdb3f0574fe024c11fb45
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-23 12:37:57 +0300

    create the post model

--- mvc/models.py
+++ mvc/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    id =models.CharField(max_length=150, primary_key=True)
+    text = models.CharField(max_length=200)
+    likes = models.CharField(max_length=100)
+    # image = models.ImageField(null=True, upload_to='icons')
+    image_url = models.URLField(max_length = 200, default=False)
+
+    
+
+
+    def __str__(self):
+        return self.likes

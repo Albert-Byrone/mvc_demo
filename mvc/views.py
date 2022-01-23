7a5eaabea881667ad9fea267b167b3defd9e70c2
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-23 12:35:57 +0300

    get all the posy and  pass them to the folder

--- mvc/views.py
+++ mvc/views.py
@@ -1,3 +1,17 @@
+from pyexpat import model
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from . import models
+from mvc.models import Post
+
+
+# data = serializers.serialize("json", models.Post.objects.all())
+# out = open('data.json', 'w')
+# out.write(data)
+# out.close()
 
 # Create your views here.
+def index(request):
+    post = Post.objects.all()
+    return render(request, 'post.html', locals())

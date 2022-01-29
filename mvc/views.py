e632369626a1fbcc70f97c4aca9328e46c71568b
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-29 16:47:47 +0300

    add an instance of post

--- mvc/views.py
+++ mvc/views.py
@@ -1,10 +1,12 @@
 from pyexpat import model
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.core import serializers
+
+from mvc import forms
 from . import models
 from mvc.models import Post
-
+from mvc.forms import PostForm
 
 # data = serializers.serialize("json", models.Post.objects.all())
 # out = open('data.json', 'w')
@@ -16,3 +18,20 @@ def home(request):
     post = Post.objects.all()
     return render(request, 'post.html', locals())
 
+
+def add_post(request):
+    form = PostForm()
+    if request.method == "POST":
+        data = request.POST
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('home')
+            except:
+                pass
+        else:
+            form = PostForm()
+    return render(request, 'index.html', {'form': form})
+        

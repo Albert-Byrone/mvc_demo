1ef9d1c6dfc21b75ac5093e0b4fe6edbcb084e83
Albert Byrone <albertbyrone1677@gmail.com>
2022-01-29 14:11:58 +0000

    changes

--- mvc/urls.py
+++ mvc/urls.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals
+from unicodedata import name
 from django.urls import path, include
 from django.conf import settings
 from . import views
@@ -7,5 +8,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.home, name='home'),
+    path('add', views.add_post, name="add")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
